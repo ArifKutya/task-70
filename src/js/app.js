539c2bed78dd802bf1b077778d2986ab737c16b1
Arif <arifkutq63@gmail.com>
2022-12-18 16:34:35 +0200

    reformat

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,19 @@
-function myFunction() {
-  let x = document.querySelector(".main");
-  x.style.transform ="scale(2)"
-}
+window.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    alert("boom")
+  });
+});
+let body = document.querySelector('body')
+let profile = document.createElement("div");
+profile.classList.add('name')
+let img = document.createElement("img")
+img.setAttribute("src", "./images/favicon.ico")
+img.classList.add('image')
+profile.appendChild(img)
+profile.appendChild(profile)
+
+const image = document.querySelector('.image')
+image.addEventListener('click', () => {
+  image.style.transform = "scale(2)"
+})
