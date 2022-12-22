86cd337f2ee340e4611738558ed03758f2885036
Arif <arifkutq63@gmail.com>
2022-12-22 16:14:33 +0200

    refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("boom")
-  });
-});
-let body = document.querySelector('body')
-let profile = document.createElement("div");
-profile.classList.add('name')
-let img = document.createElement("img")
-img.setAttribute("src", "./images/img_avatar.png")
-img.classList.add('image')
-profile.appendChild(img)
-profile.appendChild(profile)
+    const image = document.querySelector('.image')
+    image.addEventListener('click', () => {
+        image.style.transform = "scale(2)";
 
-const image = document.querySelector('.image')
-image.addEventListener('click', () => {
-  image.style.transform = "scale(2)"
-})
+    });
+});
+
